f833a2bf992fbc3f574ac2d47d8b93c7d9c0c9f2
kokovych <kokovych@gmail.com>
2018-01-25 23:33:47 +0200

    weather

--- weather_api.py
+++ weather_api.py
@@ -2,31 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-WEATHER_URL = "https://www.foreca.com/"
-
+WEATHER_URL = "https://www.foreca.com"
 city = "dnipro"
-
 param = {
     "q": city,
     "do_search": "Find+place"
 }
 
-response = requests.post(url=WEATHER_URL, data=param)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-soup = soup.find("dl", class_="in")
-soup = soup.find_all("dd")
+def get_city():
+    response = requests.post(url=WEATHER_URL, data=param)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = soup.find("dl", class_="in")
+    soup = soup.find_all("dd")
+    links = []
+    for s in soup:
+        link = WEATHER_URL + s.a.get('href')
+        links.append(link)
+    print(links)
+    return links
 
 
-links = []
+def weather_data(city_link):
+    response = requests.get(url=city_link)
+    soup = BeautifulSoup(response.content, 'html.parser' )
+    #print(soup)
+    temperature = soup.find("span", class_='cold txt-xxlarge').get_text()
+    print(temperature)
+    wind_speed = soup.find("span", class_='cold txt-xxlarge').strong.findNext("strong").get_text()
+    print(wind_speed)
+    info = soup.find("div", class_="right txt-tight").get_text()
+    print (info)
+    meteogram = soup.find("div", class_="meteogram").img['src']
+    print(meteogram)
+    print(type(meteogram))
+    meteogram_url = WEATHER_URL + meteogram
+    print(meteogram_url)
+    return temperature, wind_speed, info, meteogram_url
 
-for s in soup:
-    link = WEATHER_URL + s.a.get('href')
-    links.append(link)
 
-print(links)
+def main():
+    links = get_city()
+    temperature, wind_speed, info, meteogram_url = weather_data(links[0])
 
-response = requests.get(url=links[0])
-
-soup = BeautifulSoup(response.content, 'html.parser' )
-print(soup)
+if __name__ == "__main__":
+    main()
