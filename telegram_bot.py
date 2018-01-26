f67ba2b7704d0e706076c349f9472d75d4ecb463
kokovych <kokovych@gmail.com>
2018-01-26 18:08:10 +0200

    telebot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,78 @@
 import telebot
 import my_constants
 
+from weather_api import get_city, weather_data, WEATHER_URL
 bot = telebot.TeleBot(my_constants.token)
 
 #bot.send_message(my_constants.chat_id, "test")
 
 
+current_cities_list = []
+
+
+def change_current_cities_list(cities):
+    global current_cities_list
+    current_cities_list = cities
+    print("in def -->")
+    print(current_cities_list)
+
+
+def create_keyboard(name):
+    links, cities = get_city(name)
+    print(links)
+    print(cities)
+    # current_cities_list = cities
+    user_markup = telebot.types.ReplyKeyboardMarkup()
+    for city in cities:
+        user_markup.row(city)
+    return cities, user_markup
+    #bot.send_message(message.from_user.id, "We found next cities for you: ", reply_markup=user_markup)
+
+
+@bot.message_handler(commands=['help'])
+def handle_help_commands(message):
+    help_text = '''
+    Enter name of your city and I'll sent your your current weather and forcast picture
+    '''
+    print("HELP")
+    bot.send_message(message.from_user.id, help_text)
+
 
 @bot.message_handler(content_types=['text'])
-def hendle_text(message):
-    print(message.text)
-    print(message.from_user)
-    print(message.from_user.id)
-    print("hello")
+def handle_text(message):
+    print(current_cities_list)
+    name = message.text
+    print(name)
+    if len(name) < 3:
+        bot.send_message(message.from_user.id, "Too small quantity of letters in city name!")
+    else:
+        if "/" in name:
+            #name = name.split('/')[-1]
+            print(name)
+            temperature, wind_speed, info, meteogram_url = weather_data(WEATHER_URL+name)
+            bot.send_message(message.from_user.id, "Temperature: " + temperature)
+            bot.send_message(message.from_user.id, "Wind speed: " + wind_speed)
+            bot.send_message(message.from_user.id, "Aditional: " + info)
+            bot.send_message(message.from_user.id, "Forecast: " + meteogram_url)
+        else:
+            print ("current_cities_list")
+            print (current_cities_list)
+            links, cities =get_city(name)
+            print(links)
+            print(cities)
+            # current_cities_list = cities
+            user_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard = True)
+            user_markup.row("/help")
+            for city in cities:
+                user_markup.row(city)
+            bot.send_message(message.from_user.id, "We found next cities for you: ", reply_markup=user_markup)
+                # change_current_cities_list(cities=cities)
+                # print("in if")
+                # print(current_cities_list)
+            # else:
+            #     print("yes, in current list")
+            #     print("else ---> ")
+            #     print(current_cities_list)
 
 
 if __name__ == "__main__":
--- weather_api.py
+++ weather_api.py
@@ -11,16 +11,35 @@ def get_city(city):
         "q": city,
         "do_search": "Find+place"
     }
+    cities = []
     response = requests.post(url=WEATHER_URL, data=param)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    soup = soup.find("dl", class_="in")
-    soup = soup.find_all("dd")
-    links = []
-    for s in soup:
-        link = WEATHER_URL + s.a.get('href')
-        links.append(link)
-    print(links)
-    return links
+    html = BeautifulSoup(response.content, 'html.parser')
+    content_right = html.find('div', class_='content-right')
+    print(content_right)
+    clearb = content_right.find('div', class_='clearb').findNext('p').get_text()
+    print('\n\n\n====================\n\n\n')
+    print("clearb")
+    print(clearb)
+    print(len(clearb))
+    if clearb == "No results.":
+        cities.append("No results.")
+    soup = content_right.find("dl", class_="in")
+    if soup is not None:
+        soup = soup.find_all("dd")
+        for s in soup:
+            cities.append(s.a.get('href'))
+        # print(links)
+        # print(cities)
+        return cities
+    else:
+        print('\n\n\n====================\n\n\n')
+        city = content_right.find('form', method='post')
+        if city is not None:
+            city = city.get('action')
+            print(city)
+            cities.append(city)
+    print(cities)
+    return cities
 
 
 def weather_data(city_link):
@@ -36,12 +55,13 @@ def weather_data(city_link):
 
 
 def main():
-    links = get_city(city="dnipro")
-    temperature, wind_speed, info, meteogram_url = weather_data(links[0])
-    print(temperature)
-    print(wind_speed)
-    print (info)
-    print(meteogram_url)
+    cities = get_city(city="kievkart")
+    print(cities)
+    # temperature, wind_speed, info, meteogram_url = weather_data(links[0])
+    # print(temperature)
+    # print(wind_speed)
+    # print (info)
+    # print(meteogram_url)
 
 
 if __name__ == "__main__":
