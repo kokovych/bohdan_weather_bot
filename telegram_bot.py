bc29ba9ddf16dd2408e53e6873ce02354c471246
kokovych <kokovych@gmail.com>
2018-02-03 23:14:44 +0200

    edit bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,75 +4,85 @@ import my_constants
 from weather_api import get_city, weather_data, WEATHER_URL
 bot = telebot.TeleBot(my_constants.token)
 
-#bot.send_message(my_constants.chat_id, "test")
+weather_string = """
+TODAY WEATHER =====>>>>\nTemperature: {temperature}
+Wind speed: {wind_speed}\n
+Additional info:{additional_info}\n
 
+SHORT FORECAST =====>>>>
+TODAY
+{today_title}
+{today_minmax}
 
-current_cities_list = []
+TOMORROW
+{tomorrow_title}
+{tomorrow_minmax}
 
-
-def change_current_cities_list(cities):
-    global current_cities_list
-    current_cities_list = cities
-    print("in def -->")
-    print(current_cities_list)
-
-
-def create_keyboard(name):
-    links, cities = get_city(name)
-    print(links)
-    print(cities)
-    # current_cities_list = cities
-    user_markup = telebot.types.ReplyKeyboardMarkup()
-    for city in cities:
-        user_markup.row(city)
-    return cities, user_markup
-    #bot.send_message(message.from_user.id, "We found next cities for you: ", reply_markup=user_markup)
-
+AFTER TOMORROW
+{aftertomorrow_title}
+{aftertomorrow_minmax}
+"""
 
 @bot.message_handler(commands=['help'])
 def handle_help_commands(message):
     help_text = '''
-    Enter name of your city and I'll sent your your current weather and forcast picture
+    Enter your city and I'll send your current weather and short forecast 
     '''
-    print("HELP")
+    bot.send_message(message.from_user.id, help_text)
+
+
+@bot.message_handler(commands=['start'])
+def handle_help_commands(message):
+    help_text = '''
+    Enter  your city and I'll send your current weather and short forecast 
+    '''
     bot.send_message(message.from_user.id, help_text)
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    print(current_cities_list)
     name = message.text
-    print(name)
     if len(name) < 3:
         bot.send_message(message.from_user.id, "Too small quantity of letters in city name!")
     else:
         if "/" in name:
-            #name = name.split('/')[-1]
-            print(name)
-            temperature, wind_speed, info, meteogram_url = weather_data(WEATHER_URL+name)
-            bot.send_message(message.from_user.id, "Temperature: " + temperature)
-            bot.send_message(message.from_user.id, "Wind speed: " + wind_speed)
-            bot.send_message(message.from_user.id, "Aditional: " + info)
-            bot.send_message(message.from_user.id, "Forecast: " + meteogram_url)
+            temperature, wind_speed, info, short_forecast = weather_data(WEATHER_URL+name)
+            today_title = short_forecast.get("today_title")
+            today_minmax = short_forecast.get("today_minmax")
+
+            tomorrow_title = short_forecast.get("tomorrow_title")
+            tomorrow_minmax = short_forecast.get("tomorrow_minmax")
+
+            aftertomorrow_title = short_forecast.get("aftertomorrow_title")
+            aftertomorrow_minmax = short_forecast.get("aftertomorrow_minmax")
+
+            total_weather_string = weather_string.format(
+                temperature=temperature,
+                wind_speed=wind_speed,
+                additional_info=info,
+                today_title=today_title,
+                today_minmax=today_minmax,
+                tomorrow_title=tomorrow_title,
+                tomorrow_minmax=tomorrow_minmax,
+                aftertomorrow_title=aftertomorrow_title,
+                aftertomorrow_minmax=aftertomorrow_minmax
+            )
+
+            bot.send_message(message.from_user.id, total_weather_string)
         else:
-            print ("current_cities_list")
-            print (current_cities_list)
-            links, cities =get_city(name)
-            print(links)
-            print(cities)
-            # current_cities_list = cities
-            user_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard = True)
-            user_markup.row("/help")
-            for city in cities:
-                user_markup.row(city)
-            bot.send_message(message.from_user.id, "We found next cities for you: ", reply_markup=user_markup)
-                # change_current_cities_list(cities=cities)
-                # print("in if")
-                # print(current_cities_list)
-            # else:
-            #     print("yes, in current list")
-            #     print("else ---> ")
-            #     print(current_cities_list)
+            cities =get_city(name)
+            # no results case
+            if cities[0] == 'No results.':
+                bot.send_message(message.from_user.id, "No results.")
+            else:
+                user_markup = telebot.types.ReplyKeyboardMarkup(
+                    one_time_keyboard=True)
+                user_markup.row("/help")
+                for city in cities:
+                    user_markup.row(city)
+                bot.send_message(message.from_user.id,
+                                 "We found next cities for you: ",
+                                 reply_markup=user_markup)
 
 
 if __name__ == "__main__":
--- weather_api.py
+++ weather_api.py
@@ -1,9 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 
 WEATHER_URL = "https://www.foreca.com"
-#city = "dnipro"
 
 
 def get_city(city):
@@ -15,12 +13,7 @@ def get_city(city):
     response = requests.post(url=WEATHER_URL, data=param)
     html = BeautifulSoup(response.content, 'html.parser')
     content_right = html.find('div', class_='content-right')
-    print(content_right)
     clearb = content_right.find('div', class_='clearb').findNext('p').get_text()
-    print('\n\n\n====================\n\n\n')
-    print("clearb")
-    print(clearb)
-    print(len(clearb))
     if clearb == "No results.":
         cities.append("No results.")
     soup = content_right.find("dl", class_="in")
@@ -28,34 +21,59 @@ def get_city(city):
         soup = soup.find_all("dd")
         for s in soup:
             cities.append(s.a.get('href'))
-        # print(links)
-        # print(cities)
         return cities
     else:
-        print('\n\n\n====================\n\n\n')
         city = content_right.find('form', method='post')
         if city is not None:
             city = city.get('action')
-            print(city)
             cities.append(city)
-    print(cities)
     return cities
 
 
 def weather_data(city_link):
     response = requests.get(url=city_link)
     soup = BeautifulSoup(response.content, 'html.parser' )
-    temperature = soup.find("span", class_='cold txt-xxlarge').get_text()
-    wind_speed = soup.find("span", class_='cold txt-xxlarge').strong.findNext("strong").get_text()
+    temperature = soup.find("div", class_='left').span.get_text()
+    wind_speed = soup.find("div", class_='left').strong.findNext("strong").get_text()
     info = soup.find("div", class_="right txt-tight").get_text()
-    meteogram = soup.find("div", class_="meteogram").img['src']
-    meteogram_url = WEATHER_URL + meteogram
+    info = info.replace('\r', '').replace('\t', '').replace('\n\n', '\n')#.replace(' ', '')
+    while '  ' in info:
+        info = info.replace('  ', ' ')
+    # meteogram = soup.find("div", class_="meteogram").img['src']
+    # meteogram_url = WEATHER_URL + meteogram
+    c2 = soup.find("div", class_="c2")
+    short_forecast = {}
 
-    return temperature, wind_speed, info, meteogram_url
+    # short forecast today
+    c2_a_today = c2.find("div", class_="c2_a")
+    today_title, today_minmax = get_title_and_minmax(c2_a_today)
+    short_forecast["today_title"] = today_title
+    short_forecast["today_minmax"] = today_minmax
+
+    # short forecast tomorrow
+    c2_a_tomorrow = c2_a_today.findNext("div", class_="c2_a")
+    tomorrow_title, tomorrow_minmax = get_title_and_minmax(c2_a_tomorrow)
+    short_forecast["tomorrow_title"] = tomorrow_title
+    short_forecast["tomorrow_minmax"] = tomorrow_minmax
+
+    # short forecast after tomorrow
+    c2_a_after_tomorrow = c2_a_tomorrow.findNext("div", class_="c2_a")
+    aftertomorrow_title, aftertomorrow_minmax = get_title_and_minmax(c2_a_after_tomorrow)
+    short_forecast["aftertomorrow_title"] = aftertomorrow_title
+    short_forecast["aftertomorrow_minmax"] = aftertomorrow_minmax
+
+    return temperature, wind_speed, info, short_forecast
+
+
+def get_title_and_minmax(c2_a):
+    title = c2_a.a.get("title")
+    minmax = c2_a.find("div", id="minmax").get_text()
+    minmax = minmax.replace('\r', '').replace('\n', '')
+    return title, minmax
 
 
 def main():
-    cities = get_city(city="kievkart")
+    cities = get_city(city="dniprorrr")
     print(cities)
     # temperature, wind_speed, info, meteogram_url = weather_data(links[0])
     # print(temperature)
